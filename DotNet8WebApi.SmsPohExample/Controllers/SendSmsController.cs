797f4388b2315c458fbfc60d8963fb42aa880b41
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-06 12:06:25 +0630

    Add project files.

---
+++ DotNet8WebApi.SmsPohExample/Controllers/SendSmsController.cs
@@ -0,0 +1,48 @@
+﻿using DotNet8WebApi.SmsPohExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DotNet8WebApi.SmsPohExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendSmsController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public SendSmsController(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendSms([FromBody] SmsDto sms)
+        {
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration.GetSection("AuthKey").Value!);
+                string jsonStr = JsonConvert.SerializeObject(sms);
+                HttpContent content = new StringContent(jsonStr, Encoding.UTF8, Application.Json);
+                HttpResponseMessage response = await _httpClient.PostAsync("/api/v2/send", content);
+
+                string message = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok(message);
+                }
+
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ DotNet8WebApi.SmsPohExample/Models/SmsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8WebApi.SmsPohExample.Models
+{
+    public class SmsDto
+    {
+        public string ToMobileNumber { get; set; }
+        public string Message { get; set; }
+        public string SenderName { get; set; }
+    }
+}
